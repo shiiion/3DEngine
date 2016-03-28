0d99dc3f2cdab6d4c1c0d1790a9fede1f55655e1
Justcallmeivan <ikhokhlov@yahoo.com>
2016-03-27 23:24:27 -0400

    Added Renderable.h and .cpp. I did not add anything involving the mesh since I dot know how. I hope I did this right, you guys should probably check though.

---
+++ core/Renderable.cpp
@@ -0,0 +1,18 @@
+#include "Renderable.h"
+//Should probably double check this
+
+using namespace ginkgo;
+	Renderable::Renderable(const glm::vec3& pos, const glm::vec3& scl, const glm::vec3& rot, const glm::vec3& vel, const glm::vec3& accel) : 
+		Entity::Entity(pos, rot, vel, accel)
+	{
+		scale = scl;
+	}
+
+	const glm::vec3& Renderable::getScale() const
+	{
+		return scale;
+	}
+	void Renderable::setScale(const glm::vec3& scl)
+	{
+		scale = scl;
+	}
---
+++ core/Renderable.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Entity.h"
+//Should probably double check this
+
+namespace ginkgo {
+	class Renderable : public Entity
+	{
+	private:
+		glm::vec3 scale;
+		
+	public:
+		Renderable(const glm::vec3& pos, const glm::vec3& scl = glm::vec3(),const glm::vec3& rot = glm::vec3(), const glm::vec3& vel = glm::vec3(), const glm::vec3& accel = glm::vec3());
+
+		virtual const glm::vec3& getScale() const;
+
+		virtual void setScale(const glm::vec3& scl);
+
+		
+	};
+}
