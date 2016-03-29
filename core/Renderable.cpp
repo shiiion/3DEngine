c9d2d5b4b24b78aa63912ef5958628a2072e4895
Justcallmeivan <ikhokhlov@yahoo.com>
2016-03-28 23:04:34 -0400

    Okay okay, i got it this time

--- core/Renderable.cpp
+++ core/Renderable.cpp
@@ -29,47 +29,52 @@ namespace ginkgo
 		this->mesh = mesh;
 	}
 
-	const glm::vec3& Entity::getAcceleration() const
+	void Renderable::render()
+	{
+
+	}
+
+	const glm::vec3& Renderable::getAcceleration() const
 	{
 		return acceleration;
 	}
 
-	const glm::vec3& Entity::getVelocity() const
+	const glm::vec3& Renderable::getVelocity() const
 	{
 		return velocity;
 	}
 
-	const glm::vec3& Entity::getRotation() const
+	const glm::vec3& Renderable::getRotation() const
 	{
 		return rotation;
 	}
 
-	const glm::vec3& Entity::getPosition() const
+	const glm::vec3& Renderable::getPosition() const
 	{
 		return position;
 	}
 
-	void Entity::setAcceleration(const glm::vec3& accel)
+	void Renderable::setAcceleration(const glm::vec3& accel)
 	{
 		acceleration = accel;
 	}
 
-	void Entity::setVelocity(const glm::vec3& vel)
+	void Renderable::setVelocity(const glm::vec3& vel)
 	{
 		velocity = vel;
 	}
 
-	void Entity::setRotation(const glm::vec3& rot)
+	void Renderable::setRotation(const glm::vec3& rot)
 	{
 		rotation = rot;
 	}
 
-	void Entity::setPosition(const glm::vec3& pos)
+	void Renderable::setPosition(const glm::vec3& pos)
 	{
 		position = pos;
 	}
 
-	void Entity::tick(float elapsedTime)
+	void Renderable::tick(float elapsedTime)
 	{
 		velocity += acceleration * elapsedTime;
 		position += velocity * elapsedTime;
