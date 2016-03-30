f73b615be6ba9bbb76677531e3a8e19874f40888
Samuel <mayansymbol@gmail.com>
2016-03-29 21:24:48 -0400

    iworld change

--- core/Core.cpp
+++ core/Core.cpp
@@ -1,9 +1,11 @@
 #include "Core.h"
 #include "IWorld.h"
+#include "IEntity.h"
+#include <Windows.h>
 
 namespace ginkgo
 {
-	long Core::entityIDBase = 1;
+	volatile long Core::entityIDBase = 1;
 	Core Core::core = Core();
 
 	long Core::generateID()
@@ -11,8 +13,43 @@ namespace ginkgo
 		return entityIDBase++;
 	}
 
+	Core::Core()
+	{
+		running = false;
+		tickTime = (1.f / 60.f);
+		startTick = GetTickCount64();
+	}
+
+	float Core::getTickTime() const
+	{
+		return tickTime;
+	}
+
+	void Core::setTickTime(float time)
+	{
+		tickTime = time;
+	}
+
+	float Core::getEngineTime() const
+	{
+		return (float)(GetTickCount64() - startTick) / 1000.f;
+	}
+
 	void Core::coreThread()
 	{
-		vector<
+		running = true;
+		float tickStart = getEngineTime(), tickEnd = getEngineTime(), elapsedTime;
+		while (running)
+		{
+			const vector<IEntity*>& entityList = world->getEntityList();
+
+			for (IEntity* e : entityList)
+			{
+				e->tick(elapsedTime);
+			}
+
+
+			std::this_thread::sleep_for(tickTime * 1000.f);
+		}
 	}
 }
